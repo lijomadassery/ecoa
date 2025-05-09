bfb76a84df4718beb0a3f222394f75f0158d2abc
lijomadassery <lijo.madassery@gmail.com>
2025-05-08 20:01:17 -0700

    Fix to Entry Point

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$USE_ENVSUBST" = "true" ]; then
-  envsubst '${VITE_API_BASE_URL}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
-  mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
+  envsubst '${VITE_API_BASE_URL}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+  mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 fi
 exec nginx -g 'daemon off;' 
